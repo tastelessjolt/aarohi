1d104f3744c26603866a5fd10baa87d9c294fbdc
Varshith <varshiths@cse.iitb.ac.in>
2017-04-01 00:41:27 +0530

    Network done. Testing pending

--- src/Aarohi.py
+++ src/Aarohi.py
@@ -1,19 +1,36 @@
 import tensorflow as tf
 import numpy as np
-import wave
+import scipy.io.wavfile as wv
 import os
+
+
+def bf16(num):
+	inum = bin(np.uint16(num))
+	l = [int(b) for b in inum[2:]]
+	p = [0]*(16-len(l))
+	p.extend(l)
+	return p
+
+def f16b(bin_array):
+	stri = "".join([str(x) for x in bin_array])
+	if(stri[0] == '1'):
+		return int(stri,2) - (1<<16)
+	else:
+		return int(stri,2)
+
 
 CELLS_IN_LSTM = 100
 CASCADED_CELLS = 4
-BATCH_SIZE = 5
-MEL_BANDS = 5
-SAMPLING_RATE = 44100
+BATCH_SIZE = 10000
+MEL_BANDS = 1
+SAMPLING_RATE = 96000
 INPUT_SEC = 3
-FEATURES_PER_BAND = SAMPLING_RATE * 4 * INPUT_SEC
-MAX_SEQ_LENGTH = SAMPLING_RATE * 4 * 360
-N_CLASSES = 256
+FEATURES_PER_BAND = SAMPLING_RATE * INPUT_SEC
+MAX_SEQ_LENGTH = SAMPLING_RATE * 360
+N_CLASSES = 16
 OUTPUT_SEC = 0.5
-OUTPUT_SEQ_LENGTH = SAMPLING_RATE * 4 * OUTPUT_SEC
+OUTPUT_SEQ_LENGTH = SAMPLING_RATE * OUTPUT_SEC
+EPOCH = 5000
 
 class Aarohi():
 	"""docstring for Aarohi"""
@@ -22,64 +39,96 @@ class Aarohi():
 		cell = tf.contrib.rnn.LSTMCell(CELLS_IN_LSTM, state_is_tuple=True)
 		network = tf.contrib.rnn.MultiRNNCell([cell] * CASCADED_CELLS)
 		
-		data  = tf.placeholder(tf.float32, [None, FEATURES_PER_BAND, MEL_BANDS])
-		target = tf.placeholder(tf.float32, [None, N_CLASSES, OUTPUT_SEQ_LENGTH, MEL_BANDS])
+		data  = tf.placeholder(tf.float32, [None, FEATURES_PER_BAND, 1])
+		target = tf.placeholder(tf.float32, [None, N_CLASSES])
 
 		output, state = tf.nn.dynamic_rnn(network, data, dtype = tf.float32)
 		
-		max_len = int(target.get_shape()[1])
-		out_size = target.get_shape()[2:]
+		output = tf.transpose(output, [1, 0, 2])
+		last = tf.gather(output, int(output.get_shape()[0]) - 1)
 
-		out_dim = [CELLS_IN_LSTM]
-		out_dim.extend(out_size)
-		weight = tf.Variable(tf.truncated_normal([CELLS_IN_LSTM, out_size], stddev=0.1))
-		bias = tf.Variable(tf.constant(0.1, shape = out_size))
+		weight = tf.Variable(tf.truncated_normal([CELLS_IN_LSTM, int(target.get_shape()[1])]))
+		bias = tf.Variable(tf.constant(0.1, shape=[target.get_shape()[1]]))
 
-		output = tf.reshape(output, [-1, CELLS_IN_LSTM])
-		prediction = tf.nn.softmax(tf.matmul(output, weight) + bias)
-		pred_dim = [-1, max_len]
-		pred_dim.extend(out_size)
-		prediction = tf.reshape(prediction, pred_dim)
+		prediction = tf.nn.softmax(tf.matmul(last, weight) + bias)
+		cross_entropy = -tf.reduce_sum(target * tf.log(tf.clip_by_value(prediction,1e-10,1.0)))
 
-		cross_entropy = -tf.reduce_sum(target*tf.log(prediction), [1,2])
-		cross_entropy = tf.reduce_mean(cross_entropy)
-
-		optimiser = tf.train.AdamOptimiser()
+		optimiser = tf.train.AdamOptimizer()
 		minimize = optimiser.minimize(cross_entropy)
 
-		mistakes = tf.not_equal(tf.argmax(target, 1), tf.argmax(prediction, 1))
-		error = tf.reduce_mean(tf.cast(mistakes, tf.float32))
+		self.data_ph = data
+		self.target_ph = target
+		self.prediction = prediction
+
+		# mistakes = tf.not_equal(tf.argmax(target, 1), tf.argmax(prediction, 1))
+		# error = tf.reduce_mean(tf.cast(mistakes, tf.float32))
 
 	def setTrainingData(self, filespath):
 
 		train_data = []
 		for path, dirs, files in os.walk(filespath):
-			for file in files:
+			for file in [files[0]]:
 				print(file)
 				if ( file[len(file)-3:] == "wav" ):
-					f = wave.open(file, 'rb')
-					dat = f.readframes(f.getnframes())
-
-				f.close()
-				train_data.append(dat)
+					(rate, sig) = wv.read(filespath + file)
+				
+				train_data.append(sig.T[0])
 
 		self.train_data_raw = train_data
 
+		training_x = []
+		training_y = []
+		
+		for song in train_data:
+			print("Constructing song")
+			for i in range(0,int((song.shape[0]-FEATURES_PER_BAND-2)/10000)):
+				print("Constructing sample = " + str(i))
+				training_x.append(song[ i:i+FEATURES_PER_BAND ])
+				training_y.append(bf16(song[i+FEATURES_PER_BAND+1]) )
+
+		print("Construction done")
+		self.train_x = training_x
+		self.train_y = training_y
+
 	def train(self):
+
+		print("Training commences")
 
 		init_op = tf.initialize_all_variables()
 		sess = tf.Session()
-		# sess.run(init_op)
+		sess.run(init_op)
 
-		# no_of_batches = int(len(self.train_input)/BATCH_SIZE)
-		# epoch = 5000
-		# for i in range(epoch):
-		    # ptr = 0
-		    # for j in range(no_of_batches):
-		        # inp, out = self.train_input[ptr:ptr+BATCH_SIZE], self.train_output[ptr:ptr+BATCH_SIZE]
-		        # ptr+=BATCH_SIZE
-		        # sess.run(minimize,{data: inp, target: out})
-		    # print ("Epoch - ",str(i))
+		no_of_batches = int(len(self.train_x)/BATCH_SIZE)
+		for i in range(EPOCH):
+		    ptr = 0
+		    for j in range(no_of_batches):
+		        inp, out = self.train_x[ptr:ptr+BATCH_SIZE], self.train_y[ptr:ptr+BATCH_SIZE]
+		        ptr += BATCH_SIZE
+		        sess.run(minimize,{self.data_ph: inp, self.target_ph: out})
+		    print ("Epoch - ", str(i))
+
+		self.sess = sess
 
 	def inventSong(self):
-		pass
+
+		print("Song gen starts")
+		
+		song_length = 20
+		seed = np.array([ 0 for i in range(0,FEATURES_PER_BAND) ], dtype=np.int16)
+		print("Song seed = ", seed)
+		song = []
+
+		fram = seed
+		fram = np.reshape(fram, (-1, FEATURES_PER_BAND, 1))
+		print(fram, fram.shape)
+
+		for x in range(0,song_length * SAMPLING_RATE):
+			print("iteration",x)
+			next_pt = self.sess.run(self.prediction, {self.data_ph: fram})
+			next_pt = [ int(x > 0.5) for x in next_pt ]
+			fram_int = f16b(next_pt)
+			song.append(fram_int)
+			fram = np.append(fram, np.array([[[fram_int]]]), axis=1)
+
+		print("Generating the wav file")
+		wv.write("gen.wav", SAMPLING_RATE, song)
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 def convertOggToWav(filespath):
 
@@ -7,3 +8,8 @@ def convertOggToWav(filespath):
 			print(file)
 			if ( file[len(file)-3:] == "ogg" ):
 				os.system("oggdec -o " + filespath + "/" + file[:-4] + ".wav " + filespath + "/" + file)
+
+def binary_from_int16(num):
+	inum = bin(np.uint32(num))
+	return [int(b) for b in inum[2:]]
+	
